1c6ee3e7aba039ecdb2a30055bf26d6b20e45557
pat92fr <48148915+pat92fr@users.noreply.github.com>
2021-03-29 17:53:16 +0200

    regenerating HAL source code files from merged ioc
    I2C timing updated

--- 01-Firmware/B-G431B-ESC1/Core/Src/main.c
+++ 01-Firmware/B-G431B-ESC1/Core/Src/main.c
@@ -992,7 +992,7 @@ static void MX_I2C1_Init(void)
 
   /* USER CODE END I2C1_Init 1 */
   hi2c1.Instance = I2C1;
-  hi2c1.Init.Timing = 0x00701D64;
+  hi2c1.Init.Timing = 0x00701F6B;
   hi2c1.Init.OwnAddress1 = 0;
   hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
   hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
