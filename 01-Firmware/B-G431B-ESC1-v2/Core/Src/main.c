93d0c5ffbb640a4ae756eee60bb9ed0159c08226
pat92fr <48148915+pat92fr@users.noreply.github.com>
2021-05-17 20:09:31 +0200

    Support of STATUS LED (user controlable)

--- 01-Firmware/B-G431B-ESC1-v2/Core/Src/main.c
+++ 01-Firmware/B-G431B-ESC1-v2/Core/Src/main.c
@@ -272,6 +272,8 @@ int main(void)
     /* USER CODE BEGIN 3 */
 
 	// Handle local MMI
+	// Led STATUS
+	HAL_GPIO_WritePin(STATUS_GPIO_Port,STATUS_Pin,regs[REG_LED]>0?GPIO_PIN_SET:GPIO_PIN_RESET);
 	// Pressing the button starts calibration
 	if(HAL_GPIO_ReadPin(BUTTON_GPIO_Port,BUTTON_Pin)==GPIO_PIN_RESET)
 	{
