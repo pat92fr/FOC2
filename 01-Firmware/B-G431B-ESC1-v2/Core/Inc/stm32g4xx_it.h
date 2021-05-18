110d965cc5b46f9edfd0e8dfa096baf66b4c9816
pat92fr <48148915+pat92fr@users.noreply.github.com>
2021-05-18 17:40:26 +0200

    CAN It fix

--- 01-Firmware/B-G431B-ESC1-v2/Core/Inc/stm32g4xx_it.h
+++ 01-Firmware/B-G431B-ESC1-v2/Core/Inc/stm32g4xx_it.h
@@ -61,6 +61,7 @@ void DMA1_Channel2_IRQHandler(void);
 void DMA1_Channel3_IRQHandler(void);
 void DMA1_Channel4_IRQHandler(void);
 void ADC1_2_IRQHandler(void);
+void FDCAN1_IT0_IRQHandler(void);
 void TIM4_IRQHandler(void);
 void USART2_IRQHandler(void);
 void CORDIC_IRQHandler(void);
